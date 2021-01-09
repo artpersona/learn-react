44d9c7d208f1331d9dd29f19e590db423439217d
artpersona <jp11perez@gmail.com>
2021-01-09 22:40:30 +0800

    Initial Commit

---
+++ src/HOC/CommentCount.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function CommentCount() {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CommentCount
---
+++ src/HOC/LikeCount.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LikeCount() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LikeCount
